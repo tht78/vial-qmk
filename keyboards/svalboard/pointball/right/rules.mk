aa8fda099e04e7aa0a485991c3881e250ea73761
Morgan Venable <venable@gmail.com>
2024-05-06 16:08:58 -0700

    3389 tracking branch

--- keyboards/svalboard/pointball/right/rules.mk
+++ keyboards/svalboard/pointball/right/rules.mk
@@ -2,6 +2,6 @@ OPT_DEFS += -DINIT_EE_HANDS_RIGHT
 # this turns on trackballs including pimoroni
 POINTING_DEVICE_ENABLE = yes
 # this selects trackball driver
-POINTING_DEVICE_DRIVER = pmw3360
+POINTING_DEVICE_DRIVER = pmw3389
 # pimoroni_trackball
 # pmw3389
